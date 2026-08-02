363050b66c0f45edb8e7b272302b9a1fd6ab393d
Mohamed <mohamedanineteen@gmail.com>
2026-08-02 20:36:24 +0300

    Fix request session context

--- models/requestModel.js
+++ models/requestModel.js
@@ -1,13 +1,24 @@
 class RequestModel {
 
-    constructor(body) {
+    constructor(body = {}) {
 
         this.question = body.question || "";
 
-        this.session = body.session || {};
+        // يقبل البيانات سواء جاية باسم user أو session
+        const sessionData = body.session || body.user || {};
+
+        this.session = {
+            userName: sessionData.userName || "",
+            organizationId: sessionData.organizationId || null,
+            organizationName: sessionData.organizationName || "",
+            branchId: sessionData.branchId || null,
+            branchName: sessionData.branchName || "",
+            module: sessionData.module || "",
+            screen: sessionData.screen || "",
+            language: sessionData.language || "en"
+        };
 
         this.context = body.context || {};
-
     }
 
 }
